69fe58dc2feb88257d29f2398fe5e50d202c509d
Victor Perin <victor.perin@princiweb.com.br>
2016-03-01 18:02:57 -0300

    :sparkles: finish the version 1.0.0

--- index.js
+++ index.js
@@ -3,5 +3,11 @@
 var mochila = require('./mochila.js');
 var itens = require('./itens.js').porImportancia();
 
-console.log(itens);
+itens.forEach(function(item) {
+  if (!mochila.verificarSeEstaCheia()) {
+    mochila.adicionarItem(item);
+  }
+});
+
+console.log(mochila);
 console.log(mochila.pesoAtual());
